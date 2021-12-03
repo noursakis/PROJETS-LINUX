8285bb21f1597c24bba5689fa4cee906ca72d340
sakis Nour el houda <95090344+noursakis@users.noreply.github.com>
2021-12-03 15:51:44 +0100

    Add files via upload

--- calcul.sh
+++ calcul.sh
@@ -7,30 +7,25 @@ echo "l'opération de division de fait avec la commande DIV"
 echo "l'opération de Multiplication se fait avec la commande MUL"
 echo "l'opération de sommation se fait avec la commande SOM"
 echo "NB: faites attention de ne pas entrer un diviseur NULL"
-while [ "$calculer" != "o" -o "$calculer" != "O" ]
-do
 
-	echo -n "entrer deux nombres "
+while [ "$calculer" != "o" -o "$calculer" != "O" ]; do
+	echo -n "entrer deux nombres"
 	read A B
-	echo "choisir le type d'opération mathématique"
+	echo "chosir le type d'opération mathématique valide : "
 	read OP
-
+	while [ "$OP" != "SOUS" -a "$OP" != "SOM" -a "$OP" != "MUL" -a "$OP" != "DIV" ]; do
+		echo "Erreur ! veuillez saisir un opérateur valide !"
+		read OP
+	done
 case "$OP" in
-      SOUS )
-
-      echo echo "= $(($A-$B))";;
-
-      SOM )
-
-      echo "echo = $(($A+$B))";;
-
-      MUL ) echo "= $(($A*$B))";;
-
-      DIV ) while [ "$B" = "0" ]; do
+        SOUS ) echo "= $(($A-$B))";;
+      	SOM ) echo "= $(($A+$B))";;
+	MUL ) echo "= $(($A*$B))";;
+	DIV ) while [ "$B" = "0" ]; do
  		echo "Veuillez entrer un diviseur non nul"
  		read B
-		done
- 	    echo "= $(($A/$B))";;
+        	done
+ 	        echo "scale=2; (($A/$B))" | bc -l;;
 esac
         echo "Voulez vous continuer le calcul? O/N  "
         read  calculer
